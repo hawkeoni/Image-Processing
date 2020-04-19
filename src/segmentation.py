067dc20452a6d3bdbb14d5ff1766f48cd889df31
ilya <iliyadimov@icloud.com>
2020-04-20 01:24:44 +0300

    Add motley segmentation

--- src/segmentation.py
+++ src/segmentation.py
@@ -1,5 +1,8 @@
 import numpy as np
 from skimage import segmentation
+from skimage.color import rgb2gray
+from skimage.morphology import binary_erosion, remove_small_objects, remove_small_holes
+from skimage.measure import label
 from PIL import Image
 
 
@@ -9,4 +12,13 @@ def felzenszwalb(image: Image) -> np.ndarray:
     )
 
 
-SEGMENTATION_TECHNIQUES = {"felzenszwalb": felzenszwalb}
+def motley_segmentation(image: Image) -> np.ndarray:
+    image = np.array(image)
+    grayscale = rgb2gray(image)
+    grayscale_bin = binary_erosion(grayscale > 0.28, selem=np.ones((10, 10)))
+    grayscale_bin = remove_small_objects(grayscale_bin, min_size=image.shape[0] * image.shape[1] // 200)
+    grayscale_bin = remove_small_holes(grayscale_bin, image.shape[0] * image.shape[1] // 900)
+    return label(grayscale_bin, background=0)
+
+
+SEGMENTATION_TECHNIQUES = {"monochrome": felzenszwalb, "motley": motley_segmentation}
